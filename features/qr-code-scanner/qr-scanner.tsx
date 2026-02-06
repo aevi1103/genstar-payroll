abcf0acef47c2bd98f153ac86f57f6b3b23b634e
aevi1103 <aevirontos@gmail.com>
2026-02-05 21:00:58 -0500

    feat(qr-scanner): add status messages to improve user feedback during clock in/out process
    fix(qr-scanner): update toast messages for better clarity and consistency

--- features/qr-code-scanner/qr-scanner.tsx
+++ features/qr-code-scanner/qr-scanner.tsx
@@ -1,37 +1,38 @@
 "use client";
 import { type IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
 import { toast } from "sonner";
 import { clockInOut } from "@/app/payroll/entry/actions";
 
 export const QrScanner = ({ validUrl }: { validUrl: string }) => {
+	const [status, setStatus] = useState<string>("Initializing...");
+
 	const handleClockInOut = async () => {
 		try {
 			if ("geolocation" in navigator) {
-				toast.info("Getting your location...");
+				setStatus("Getting your location...");
 				navigator.geolocation.getCurrentPosition(
 					async (position) => {
 						const { latitude, longitude } = position.coords;
-						toast.info("Processing clock in/out...");
+						setStatus("Processing clock in/out...");
 						await clockInOut(latitude, longitude);
-						toast.success("Clock in/out successful.");
 					},
 					async (error) => {
 						console.warn("Geolocation error:", error);
-						toast.info("Processing clock in/out...");
+						setStatus("Processing clock in/out...");
 						// Proceed without GPS if permission denied
 						await clockInOut();
-						toast.success("Clock in/out successful.");
 					},
 					{ timeout: 5000 },
 				);
 			} else {
 				// Browser doesn't support geolocation
-				toast.info("Processing clock in/out...");
+				setStatus("Processing clock in/out...");
 				await clockInOut();
 			}
 		} catch (error) {
 			console.error("Error during clock in/out:", error);
-			toast.error("Error during clock in/out.");
+			toast.error("Error during clock in/out");
 		}
 	};
 
@@ -41,12 +42,16 @@ export const QrScanner = ({ validUrl }: { validUrl: string }) => {
 		);
 
 		if (!hasValidUrl) {
-			toast("Invalid QR code scanned.");
+			toast.error("No valid QR code detected");
 			return;
 		}
 
 		await handleClockInOut();
 	};
+
+	if (status) {
+		<h2>{status}</h2>;
+	}
 
 	return (
 		<Scanner onScan={handleScan} onError={(error) => console.log(error)} />
